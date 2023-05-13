476e74b4057daa19d9a2793618360a47ad3b1687
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-13 10:04:18 -0300

    Construtor adicionado e modificação no metódo atualizar

--- src/main/java/br/edu/unifio/Ex5/ContaPoupanca.java
+++ src/main/java/br/edu/unifio/Ex5/ContaPoupanca.java
@@ -2,7 +2,15 @@ package br.edu.unifio.Ex5;
 
 public class ContaPoupanca extends Conta {
 
+    public ContaPoupanca() {
+        super();
+    }
+
+    public ContaPoupanca(double saldo) {
+        super(saldo);
+    }
+
     public void atualizar(double taxa) {
-        this.saldo += this.saldo * taxa * 3;
+        this.saldo -= this.saldo * taxa * 3;
     }
 }
