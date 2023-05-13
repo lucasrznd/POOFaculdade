4c4aacddba7f88aa59991a5bfd1284bc9a16242d
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-13 10:03:36 -0300

    Modificação nos metódos atualizar e depositar

--- src/main/java/br/edu/unifio/Ex5/ContaCorrente.java
+++ src/main/java/br/edu/unifio/Ex5/ContaCorrente.java
@@ -12,11 +12,11 @@ public class ContaCorrente extends Conta{
 
     @Override
     public void atualizar(double taxa) {
-        this.saldo += this.saldo * taxa * 2;
+        this.saldo -= this.saldo * taxa * 2;
     }
 
     @Override
     public void depositar(double valor) {
-        this.saldo += valor - 0.10;
+        this.saldo += valor - 0.1;
     }
 }
