91e9ca57143d8de0b0ef73c4cc17ab5b44e53910
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-13 09:46:48 -0300

    Construtor vazio e com argumentos

--- src/main/java/br/edu/unifio/Ex5/ContaCorrente.java
+++ src/main/java/br/edu/unifio/Ex5/ContaCorrente.java
@@ -2,6 +2,14 @@ package br.edu.unifio.Ex5;
 
 public class ContaCorrente extends Conta{
 
+    public ContaCorrente() {
+        super();
+    }
+
+    public ContaCorrente(double saldo) {
+        super(saldo);
+    }
+
     @Override
     public void atualizar(double taxa) {
         this.saldo += this.saldo * taxa * 2;
