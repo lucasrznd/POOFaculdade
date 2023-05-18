26367f7718634dca0cc9fd404ba6bb1905a4db6b
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-17 21:35:00 -0300

    Adicionado classe Pessoa

---
+++ src/main/java/br/edu/unifio/Ex6/Pessoa.java
@@ -0,0 +1,35 @@
+package br.edu.unifio.Ex6;
+
+public class Pessoa {
+    private String nome;
+    private Data dataNascimento;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(String nome, Data dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Data getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Data dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void imprimirDados() {
+        
+    }
+}
