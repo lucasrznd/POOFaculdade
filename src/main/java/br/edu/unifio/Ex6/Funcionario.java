22cfb3d3fc184fcca86b456fdbc4967eee0b33f2
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-17 21:46:00 -0300

    Criação da classe

---
+++ src/main/java/br/edu/unifio/Ex6/Funcionario.java
@@ -0,0 +1,32 @@
+package br.edu.unifio.Ex6;
+
+public class Funcionario extends Pessoa{
+    private double salario;
+
+    public Funcionario() {
+        super();
+    }
+
+    public Funcionario(String nome, Data dataNascimento, double salario) {
+        super(nome, dataNascimento);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularImposto() {
+        return getSalario() * 0.03;
+    }
+
+    @Override
+    public void imprimirDados() {
+        super.imprimirDados();
+        System.out.println("\nSalário: " + (getSalario() -   calcularImposto()));
+    }
+}
