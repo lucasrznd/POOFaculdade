bcf8979a7cd10edb5a19af737dcfeca2ad9bebc2
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-17 21:38:35 -0300

    Criado classe Cliente

---
+++ src/main/java/br/edu/unifio/Ex6/Cliente.java
@@ -0,0 +1,27 @@
+package br.edu.unifio.Ex6;
+
+public class Cliente extends Pessoa {
+    private int codigo;
+
+    public Cliente() {
+        super();
+    }
+
+    public Cliente(String nome, Data dataNascimento, int codigo) {
+        super(nome, dataNascimento);
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public void imprimirDados() {
+        
+    }
+}
